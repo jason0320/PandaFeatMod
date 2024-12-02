71c60c0b024aa7dc1d1a1ff49507c02d98c72889
jason0320 <jasonnosaj0320@gmail.com>
2024-12-02 19:49:04 +0800

    加入專案檔案。

---
+++ FeatPatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Mod_PandaFeatMod
+{
+    internal class FeatPatch
+    {
+        public static void OnStartCore()
+        {
+            SourceManager sources = Core.Instance.sources;
+            foreach (SourceElement.Row row in sources.elements.rows)
+            {
+                string alias = row.alias;
+                string text = alias;
+                FeatPatch.FeatRewrite(row);
+            }
+        }
+        public static void FeatRewrite(SourceElement.Row ele)
+        {
+            string[] array = new string[]
+            {
+                "Rapid"
+            };
+
+            foreach (string text in array)
+            {
+                if (ele.alias.Contains(text) && ele.alias.Contains("feat"))
+                {
+                    ele.categorySub = "special";
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Mod_PandaFeatMod.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Mod_PandaFeatMod
+{
+
+    [BepInPlugin("panda.feat.mod", "Panda's Feat Mod", "1.0.0.0")]
+    public class Mod_PandaFeatMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's Feat Mod");
+            harmony.PatchAll();
+        }
+        public void OnStartCore()
+        {
+            FeatPatch.OnStartCore();
+        }
+    }
+}
