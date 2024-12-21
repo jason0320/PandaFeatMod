6007c82105df5272751a4bff654934574feaaa6f
jason0320 <jasonnosaj0320@gmail.com>
2024-12-21 20:25:11 +0800

    Update

--- CharaListAvailabeFeatsPatch.cs
+++ CharaListAvailabeFeatsPatch.cs
@@ -12,13 +12,13 @@ class CharaListAvailabeFeatsPatch
 {
     [HarmonyPostfix]
     [HarmonyPatch(typeof(Chara), nameof(Chara.ListAvailabeFeats))]
-    internal static void Postfix(ref List<Element> __result)
+    internal static void Postfix(ref List<Element> __result, bool pet, bool showAll)
     {
         foreach (SourceElement.Row item in EClass.sources.elements.rows.Where((SourceElement.Row a) => a.group == "FEAT" && a.cost[0] != -1 && !a.categorySub.IsEmpty()))
         {
             Feat feat = Act.CC.elements.GetOrCreateElement(item.id) as Feat;
             int num = ((feat.ValueWithoutLink <= 0) ? 1 : (feat.ValueWithoutLink + 1));
-            if (num <= feat.source.max && (feat.HasTag("class") || feat.HasTag("innate")) && feat.IsAvailable(Act.CC.elements, feat.Value))
+            if (num <= feat.source.max && (feat.HasTag("class") || feat.HasTag("hidden") ||feat.HasTag("innate")) && (!pet || !feat.HasTag("noPet")) && (showAll || feat.IsPurchaseFeatReqMet(Act.CC.elements)))
             {
                 __result.Add(Element.Create(feat.id, num) as Feat);
             }
