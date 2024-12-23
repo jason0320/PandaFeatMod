a4441f247a1ef233add60cc0aceb44f11d7402fa
jason0320 <jasonnosaj0320@gmail.com>
2024-12-23 18:47:51 +0800

    Code optimization

--- CharaListAvailabeFeatsPatch.cs
+++ CharaListAvailabeFeatsPatch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,19 +11,23 @@ using static ActPlan;
 [HarmonyPatch]
 class CharaListAvailabeFeatsPatch
 {
-    [HarmonyPostfix]
+    [HarmonyTranspiler]
     [HarmonyPatch(typeof(Chara), nameof(Chara.ListAvailabeFeats))]
-    internal static void Postfix(ref List<Element> __result, bool pet, bool showAll)
+    internal static IEnumerable<CodeInstruction> OnDramaInviteIl(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
-        foreach (SourceElement.Row item in EClass.sources.elements.rows.Where((SourceElement.Row a) => a.group == "FEAT" && a.cost[0] != -1 && !a.categorySub.IsEmpty()))
-        {
-            Feat feat = Act.CC.elements.GetOrCreateElement(item.id) as Feat;
-            int num = ((feat.ValueWithoutLink <= 0) ? 1 : (feat.ValueWithoutLink + 1));
-            if (num <= feat.source.max && (feat.HasTag("class") || feat.HasTag("hidden") || feat.HasTag("innate")) && (!pet || !feat.HasTag("noPet")) && (showAll || feat.IsPurchaseFeatReqMet(Act.CC.elements)))
-            {
-                __result.Add(Element.Create(feat.id, num) as Feat);
-            }
-        }
+        return new CodeMatcher(instructions, generator)
+            .MatchStartForward(
+                new CodeMatch(OpCodes.Ldarg_1),
+                new CodeMatch(OpCodes.Brfalse),
+                new CodeMatch(OpCodes.Ldloc_3),
+                new CodeMatch(OpCodes.Ldstr, "noPet"))
+            .CreateLabel(out var label)
+            .MatchStartBackwards(
+                new CodeMatch(OpCodes.Ldloc_3),
+                new CodeMatch(OpCodes.Ldstr, "class"))
+            .InsertAndAdvance(
+                new CodeInstruction(OpCodes.Br, label))
+            .InstructionEnumeration();
     }
 }
 
